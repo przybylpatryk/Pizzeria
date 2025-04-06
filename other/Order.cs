ecfa8ea6128d378fc726019c6ef9294538413bb3
Kacper <hardkapi0707@gmail.com>
2025-04-06 19:31:38 +0200

    Pizza i Order

--- other/Order.cs
+++ other/Order.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using Pizzeria.pizza;
+using Pizzeria.users;
+using System;
 using System.Security.Cryptography.X509Certificates;
 
-namespace Pizzeria.Order
+namespace Pizzeria.order
 {
     public class Order 
     {
         public int Id { get; set; }
-        
-        public Order()
+        public Pizza Pizza { get; set; }
+        public Worker AssignedWorker {  get; set; }
+        public Client AssignedClient { get; set; }
+        private DateTime date;
+
+        public DateTime DateOfOrder
         {
-            
+            get { return date.Date; }
+            set { date = value.Date; }
+        }
+        public Order(Pizza Pizza, Client AssignedClient, DateTime DateOfOrder)
+        {
+            this.Pizza = Pizza;
+            this.AssignedClient = AssignedClient;
+            this.DateOfOrder = DateOfOrder;
         }
     }
 }
